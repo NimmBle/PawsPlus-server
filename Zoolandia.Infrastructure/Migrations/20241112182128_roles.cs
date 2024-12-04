9b9ee4304ee928e19a1c50e7079329dbf9d763ad
Hristo Panev <hristopanev20@gmail.com>
2024-12-04 23:39:33 +0200

    Removed the insertion of the roles from a previous migration and left only the DataSeed class

--- Zoolandia.Infrastructure/Migrations/20241112182128_roles.cs
+++ Zoolandia.Infrastructure/Migrations/20241112182128_roles.cs
@@ -12,25 +12,6 @@ namespace Zoolandia.Infrastructure.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[,]
-                {
-                    { "09247778-70aa-4668-93c3-7d7e8e01b238", "09247778-70aa-4668-93c3-7d7e8e01b238", "Administrator", "ADMINISTRATOR" },
-                    { "29781d47-7738-4588-9d69-98a9fc466477", "29781d47-7738-4588-9d69-98a9fc466477", "Sitter", "SITTER" },
-                    { "6360401a-91de-4e52-858d-263b0999f0a0", "6360401a-91de-4e52-858d-263b0999f0a0", "Owner", "OWNER" }
-                });
-
-            migrationBuilder.InsertData(
-                table: "AspNetUsers",
-                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Discriminator", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
-                values: new object[] { "0ce87a78-7146-4702-be5a-5944e4592d50", 0, "130054d8-b926-4639-a65d-3425a6c5af5b", "User", "hristopanev20@gmail.com", true, false, null, "HRISTOPANEV20@GMAIL.COM", "ADMIN", "AQAAAAIAAYagAAAAEBqkXGxRaEEeqb5f+sgzwxkwAz3oQsF3u2gKZxiA07clBiRXva702USiFfxqTsmoLA==", null, false, "1bf6766c-2193-44ed-b500-62b76d2369d1", false, "admin" });
-
-            migrationBuilder.InsertData(
-                table: "AspNetUserRoles",
-                columns: new[] { "RoleId", "UserId" },
-                values: new object[] { "09247778-70aa-4668-93c3-7d7e8e01b238", "0ce87a78-7146-4702-be5a-5944e4592d50" });
         }
 
         /// <inheritdoc />
