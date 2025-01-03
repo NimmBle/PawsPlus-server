64a171a97bdb03b59acd1b55add84a32161a2590
Hristo Panev <hristopanev20@gmail.com>
2025-01-03 18:33:34 +0200

    IServiceRepository FindByName implementation

--- Zoolandia.Infrastructure/Repositories/ServiceRepository.cs
+++ Zoolandia.Infrastructure/Repositories/ServiceRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Zoolandia.Application.Features.Service;
 using Zoolandia.Domain.Models;
 using Zoolandia.Domain.Repositories;
@@ -6,14 +7,26 @@ using Zoolandia.Infrastructure.Common.Persistence;
 
 namespace Zoolandia.Infrastructure.Repositories;
 
-public class ServiceRepository(ZoolandiaDbContext db)
+public class ServiceRepository(
+    ZoolandiaDbContext db,
+    IMapper mapper)
     : DataRepository<ZoolandiaDbContext, Service>(db),
         IServiceDomainRepository,
         IServiceQueryRepository
 {
-    public async Task<Service> GetServiceByName(string serviceName)
+    public async Task<Service> FindByName(string serviceName)
         => await db
             .Services
             .Where(s => s.Name == serviceName)
+            .FirstOrDefaultAsync();
+
+    
+    
+    // Not working!!!
+    public async Task<ServiceOutputModel> FindById(string serviceId)
+        => await mapper
+            .ProjectTo<ServiceOutputModel>(this
+                .All()
+                .Where(s => s.Id == serviceId))
             .FirstOrDefaultAsync();
 }
