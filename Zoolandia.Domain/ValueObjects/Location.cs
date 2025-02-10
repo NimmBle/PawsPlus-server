28107f74a0fa4d7da8571285bde1990beb0ead9d
Hristo Panev <hristopanev20@gmail.com>
2025-02-10 11:57:58 +0200

    profile location - 2nd

---
+++ Zoolandia.Domain/ValueObjects/Location.cs
@@ -0,0 +1,35 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace Zoolandia.Domain.ValueObjects;
+
+public record Location
+{
+    private Location()
+    {
+    }
+    
+    public Location(string placeId,
+        double latitude,
+        double longitude)
+    {
+        this.PlaceId = placeId;
+        this.Point = new Point(latitude, longitude) { SRID = 4326 };
+    }
+    
+    public string? PlaceId { get; init; }
+    
+    public Point? Point { get; init; }
+
+    public double GetDistanceInKilometers(double latitude, double longitude)
+    {
+        if ( Point == null )
+            return double.NaN;
+        
+        var newPoint = new Point(latitude, longitude) { SRID = 4326 };
+        
+        var distance = this.Point.Distance(newPoint) * 100;
+        
+        return distance;
+    }
+    
+}
